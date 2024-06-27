82fe616721b240aa9d59df8dd03ed9fab0aed8be
unknown <dhruvsaraswat907@gmail.com>
2024-06-27 08:55:18 +0530

    Final project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Navbar from './Components/Navbar';
+import NewsBoard from './Components/NewsBoard';
+import { useState } from 'react';
 
 function App() {
+  const [category,setCategory] = useState("general");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar isDarkMode = "dark" setCategory={setCategory}/>
+      <NewsBoard category={category}/>
     </div>
   );
 }
